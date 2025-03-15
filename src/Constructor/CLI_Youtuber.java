132f461bf137764042d42759527129a2233eaf27
alifatho69 <fauzan88ganteng@gmail.com>
2025-03-15 12:47:22 +0700

    Konstruktor

--- src/Constructor/CLI_Youtuber.java
+++ src/Constructor/CLI_Youtuber.java
@@ -2,14 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ClassObject;
+package Constructor;
 
 /**
  *
  * @author Fauzan
  */
 public class CLI_Youtuber {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    
         Youtuber ytbr = new Youtuber();
 
         ytbr.dataNamaCh("Lord Endo");
